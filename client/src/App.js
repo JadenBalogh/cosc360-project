333ad08a16adbad45941a31edc4dac32b99d0905
Jaden Balogh <jadenbalogh@gmail.com>
2021-03-18 16:27:50 -0700

    Add tailwind and API call example

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  function apiTest() {
+    axios.get('http://localhost:3001/api-test').then((response) => {
+      console.log(response.data);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiTest}>API Test</button>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api-test', (req, res) => {
+  console.log('Hey it works');
+  res.send('It worked!');
 });
 
 app.listen(port, () => {
