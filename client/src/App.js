cc8d84b1fb65c667376fd9dff123416d06852c70
Carson Ricca <carsonricca28@gmail.com>
2021-03-19 11:46:52 -0700

    Issue #3: Implement Basic Login/Register UI
    - Implement Login.
    - Implement Register.
    - Submission Prints to Console.
    - Basic Homepage to Showcase Routing.
    - Basic Header for Routing.
    - Implement Routing Structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
+import {Switch, Route, Link} from "react-router-dom";
+import Login from './components/login';
+import Register from './components/register';
+import Homepage from "./components/homepage";
 
 function App() {
-  function apiTest() {
-    axios.get('http://localhost:3001/api-test').then((response) => {
-      console.log(response.data);
-    });
-  }
+    // Example API GET Method
+    // function apiTest() {
+    //     axios.get('http://localhost:3001/api-test').then((response) => {
+    //         console.log(response.data);
+    //     });
+    // }
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={apiTest}>API Test</button>
-      </header>
-    </div>
-  );
+    return (
+        <body className="bg-gray-50">
+        <div>
+        <nav className="w-full relative flex flex-wrap items-center justify-between navbar-expand-lg">
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between bg-white">
+                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                    <Link to="/" className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black">Home</Link>
+                    <Link to="/login" className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black">Login</Link>
+                    <Link to="/register" className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black">Register</Link>
+                </div>
+            </div>
+        </nav>
+            <Switch>
+                <Route exact path='/' component={Homepage} />
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route component={Error} />
+            </Switch>
+        </div>
+        </body>
+    );
+
 }
 
 export default App;
---
+++ client/src/components/login.js
@@ -0,0 +1,56 @@
+import {Component} from 'react';
+import {Link} from "react-router-dom";
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+    changeHandler = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+    submit = (event) => {
+        event.preventDefault();
+        console.log(this.state.email);
+        console.log(this.state.password);
+    }
+    render() {
+        return (
+            <div className="w-full flex flex-wrap mt-7">
+                <div className="w-full flex flex-col">
+                    <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                            <h2 className="text-2xl font-bold mb-2 text-gray-800">Login</h2>
+                            <form className="flex flex-col pt-3 md:pt-8" onSubmit={this.submit}>
+                                <div className="flex flex-col pt-4">
+                                    <label htmlFor="email" className="text-lg">Email</label>
+                                    <input type="email" id="email" name="email" placeholder="your@email.com" onChange={this.changeHandler}
+                                           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
+                                </div>
+                                <div className="flex flex-col pt-4">
+                                    <label htmlFor="password" className="text-lg">Password</label>
+                                    <input type="password" id="password" name="password" placeholder="Password" onChange={this.changeHandler}
+                                           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
+                                </div>
+                                <input type="submit" value="Log In" className="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8"/>
+                            </form>
+                            <div className="text-center pt-12 pb-12 justify-center">
+                                <div className="m-1"> Don't have an account?</div>
+                                <Link to="/register" className="underline font-semibold">Register here.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
