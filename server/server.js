5daa26de01a25a9933a1dcb7ea5746c3f9d3c828
Jaden Balogh <jadenbalogh@gmail.com>
2021-03-22 14:02:33 -0700

    Add static build serving

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+const __dirname = path.resolve();
+
 import express from 'express';
 import cors from 'cors';
 import { jwtAuthenticationMiddleware, isAuthenticatedMiddleware } from './accounts/middlewares.js';
@@ -13,6 +16,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(jwtAuthenticationMiddleware);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.post('/accounts/login', login);
 app.post('/accounts/signup', signup);
