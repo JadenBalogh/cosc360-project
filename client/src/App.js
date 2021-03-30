e7be9328601848db9316383cb78558df0a634974
Mathew de Vin <mathewdevin03@gmail.com>
2021-03-29 21:15:51 -0700

    Created a post edit/create component
    Created a file 'PostEntry.js' that can act as a component whether it needs to be for creating a new post or editing a previously existing post.
    
    Created 'CreatePost.js' which will be a gateway for creating a post.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Homepage from './components/Homepage';
 import Header from './components/Header';
+import CreatePost from "./components/CreatePost";
 import ViewPost from "./components/ViewPost";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route exact path='/' component={Homepage}/>
           <Route path='/login' component={Login}/>
           <Route path='/register' component={Register}/>
+          <Route path='/create' component={CreatePost}/>
           <Route path='/view' component={ViewPost}/>
           <Route component={Error}/>
         </Switch>
