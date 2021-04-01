8cccea840acdfe7120574adcb1ab15854175d5a7
Jaden Balogh <jadenbalogh@gmail.com>
2021-03-31 21:09:45 -0700

    Remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
