71b79855d48b5a2ab4c779a8a84742ccc3e30750
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-01 12:46:23 -0700

    Created a simple backend for posts
    Added a simple backend for the post that allows for the app to get, create, update posts. The new get will allow an id to be given which will return a specific post. The create and update will do as expected.

--- server/feed/dao.js
+++ server/feed/dao.js
@@ -74,3 +74,19 @@ export async function updatePost(id, attributes) {
     },
   });
 }
+
+export async function getPostByID(id) {
+  return Post.findAll({
+    where: {
+      postId: id
+    }
+  })
+}
+
+export async function newPost(attributes) {
+  console.log("new Post", attributes);
+}
+
+export async function updatePost(id, attributes) {
+  console.log("update Post", id, attributes);
+}
