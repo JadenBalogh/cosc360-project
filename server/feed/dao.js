7d40e96e3d87dcf777c9f0f5809af6db57734766
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-01 18:28:16 -0700

    Minor bug fixes.
    Fixed a bug where the data wouldn't be able to get put because the server url wasn't set as a put method, but a get method instead.
    
    There was a problem with the post entry file that caused an error when editing the form due to values being set as null or undefined. States are set to update when the prop changes, not sure if there is a better way to do this?

--- server/feed/dao.js
+++ server/feed/dao.js
@@ -23,17 +23,41 @@ export async function getAllComments() {
 }
 
 export async function getPostByID(id) {
-  return Post.findAll({
-    where: {
-      postId: id
+  return {
+    post: {
+      title: 'Test',
+      link: '',
+      image: null,
+      subject: 'Testing 123 Testing 123'
     }
-  })
+  }
+  // return Post.findAll({
+  //   where: {
+  //     postId: id
+  //   }
+  // });
 }
 
 export async function newPost(attributes) {
   console.log("new Post", attributes);
+  return {
+    post: {
+      title: 'Test',
+      link: '',
+      image: null,
+      subject: 'Testing 123 Testing 123'
+    }
+  }
 }
 
 export async function updatePost(id, attributes) {
   console.log("update Post", id, attributes);
+  return {
+    post: {
+      title: 'Test',
+      link: '',
+      image: null,
+      subject: 'Testing 123 Testing 123'
+    }
+  }
 }
