9350710a2b0e1e9bfb7537f03bf2bda6d3e2a345
Carson Ricca <carsonricca28@gmail.com>
2021-04-06 09:56:03 -0700

    Update Password Recovery

--- client/src/components/PasswordRecovery.js
+++ client/src/components/PasswordRecovery.js
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom";
 import axios from "axios";
+import { useState } from "react";
+import {history} from "../_helpers";
 
 function PasswordRecovery() {
-    const passwordURL = `${process.env.REACT_APP_HOST || ""}/accounts/password-recovery`;
-    axios.post(passwordURL, {user: 'carsonricca28@gmail.com', password: 'password'}).then((response) => {
-       console.log(response);
-    });
+  const [email, setEmail] = useState("");
+  const passwordURL = `${process.env.REACT_APP_HOST || ""}/accounts/password-recovery`;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(passwordURL, {
+        userEmail: email,
+      })
+      .then((response) => {
+        console.log(response);
+          history.push("/login");
+          window.location.reload(false);
+      });
+  };
   return (
     <>
       <div
@@ -19,15 +31,24 @@ function PasswordRecovery() {
       <div className="min-h-screen flex flex-col justify-center items-center relative -mt-20">
         <div className="container max-w-md sm:bg-white sm:border border-gray-300 sm:rounded-2xl sm:shadow-xl p-6">
           <h2 className="text-2xl font-medium text-black text-center py-5">
-            Password Recovery Email Sent
+            Enter Email
           </h2>
-          <div className="text-black text-center py-5">
-            An email with your login details has been sent to the email that was
-            provided.
-          </div>
-          <div className="text-center font-normal text-blue-500 hover:text-blue-700 hover:underline">
-            <Link to="/login">Login</Link>
-          </div>
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+              required
+              className="shadow-inner appearance-none border border-gray-300 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:ring mt-5"
+            />
+              <input
+                  type="submit"
+                  value="Send Recovery Email"
+                  className="bg-gray-600 focus:bg-gray-600 cursor-pointer focus:outline-none text-white shadow-md text-lg hover:bg-gray-700 p-2 mt-4 rounded"
+              />
+          </form>
         </div>
       </div>
     </>
--- server/accounts/views.js
+++ server/accounts/views.js
@@ -93,32 +93,35 @@ export async function activateUser(req, res) {
 }
 
 export async function resetPassword(req, res) {
-  const { user, password } = req.body;
-  const email = process.env.EMAIL;
-  const emailPassword = process.env.PASSWORD;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: email,
-      pass: emailPassword,
-    },
-  });
+  const { userEmail } = req.body;
+  const user = await findUser(userEmail, null);
+  if(user) {
+    const email = process.env.EMAIL;
+    const emailPassword = process.env.PASSWORD;
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: email,
+        pass: emailPassword,
+      },
+    });
 
-  const mailOptions = {
-    from: email,
-    to: user,
-    subject: "Password Recovery",
-    text: "Here is the password for your account: " + password,
-  };
+    const mailOptions = {
+      from: email,
+      to: userEmail,
+      subject: "Password Recovery",
+      text: "Here is the password for your account: " + user.password,
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      return res.json("The email was not sent successfully");
-    } else {
-      console.log("Email sent: " + info.response);
-      res.status(200);
-      return res.json(mailOptions);
-    }
-  });
+    await transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        return res.json("The email was not sent successfully");
+      } else {
+        console.log("Email sent: " + info.response);
+        res.status(200);
+        return res.json(mailOptions);
+      }
+    });
+  }
 }
