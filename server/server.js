d4cd78e296b78482bd0da396b39159a9ee0dbe33
Jaden Balogh <jadenbalogh@gmail.com>
2021-04-14 20:58:18 -0700

    Add basic logging

--- server/server.js
+++ server/server.js
@@ -45,6 +45,7 @@ app.use(
 app.use(jwtAuthenticationMiddleware);
 
 if (process.env.NODE_ENV === "production") {
+  console.log("yo.");
   app.use(express.static(path.join(__dirname, "build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
